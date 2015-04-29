a6c24316a606680c48706b35e7075086f0dc4dc7
santiagoppena <santiago.ppena@alumnos.upm.es>
2015-04-29 11:19:41 +0200

    Despliegue en Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var author = require('../controllers/author');
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
@@ -8,6 +8,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author' , author.author);
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
 	
